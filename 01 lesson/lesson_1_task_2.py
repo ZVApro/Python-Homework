e964d2732593bddda7e0cdaef3cd2180f3db6281
Vera <vera620078@gmail.com>
2026-01-05 22:08:22 +0500

    выполнение домашнего задания №1 Pyton

---
+++ 01 lesson/lesson_1_task_2.py
@@ -0,0 +1,4 @@
+my_age=46
+print(my_age)
+my_age=(46+3)
+print(my_age)
---
+++ 01 lesson/lesson_1_task_5.py
@@ -0,0 +1,13 @@
+def num():
+    print(8, end='')
+print(8, end="")
+print(0, end="")
+print(0, end="")
+print(5, end="")
+print(5, end="")
+print(5, end="")
+print(3, end="")
+print(5, end="")
+print(3, end="")
+print(5, end="")
+num()
