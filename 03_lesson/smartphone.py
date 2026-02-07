5641980977837b08a7e45e912a6db650ffa3de41
Vera <vera620078@gmail.com>
2026-02-07 21:41:32 +0500

    выполнение домашнего задания №3 Python

---
+++ 03_lesson/smartphone.py
@@ -0,0 +1,5 @@
+class Smartphone:
+    def __init__(self, brand: object, model: object, phone_number: object):
+        self.brand = brand
+        self.model = model
+        self.phone_number = phone_number
