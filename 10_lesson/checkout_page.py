4ceb02c89f3da22eb9091d0e7e5c84be82c533ba
Vera <vera620078@gmail.com>
2026-03-28 17:13:10 +0500

    выполнение ДЗ №10 Отчетность в Allure

---
+++ 10_lesson/checkout_page.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class CheckoutPage:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+
+    def params_user(self, first_name, last_name, postal_code):
+        self.driver.find_element(
+            By.CSS_SELECTOR, '[id="first-name"]').send_keys(first_name)
+        self.driver.find_element(
+            By.CSS_SELECTOR, '[id="last-name"]').send_keys(last_name)
+        self.driver.find_element(
+            By.CSS_SELECTOR, '[id="postal-code"]').send_keys(postal_code)
+
+    def continue_button(self):
+        self.wait.until(
+            EC.element_to_be_clickable(
+                (By.CSS_SELECTOR, '[id="continue"]'))).click()
